be6467be9f21f0b6de5149d836192a1cfc74c32e
Tahateber95 <mr.layto@gmail.com>
2024-05-11 15:56:15 +0100

    Add template, user, and post params classes

---
+++ lib/core/params/params.dart
@@ -0,0 +1,14 @@
+class TemplateParams {
+  final String id;
+  TemplateParams({required this.id});
+}
+
+class UserParams {
+  final String id;
+  UserParams({required this.id});
+}
+
+class PostParams {
+  final String id;
+  PostParams({required this.id});
+}
